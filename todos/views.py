d70d7eba4d3345ba42e76791e773df8fbf296f71
Grigori Kartashyan <grigori.kartashyan@gmail.com>
2017-08-31 13:41:41 +0400

    Show todos from DB

--- todos/views.py
+++ todos/views.py
@@ -3,8 +3,14 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todo
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    todos = Todo.objects.all()[:10]
+    context = {
+            'name':'Grigori',
+            'todos': todos
+            }
+    return render(request, 'index.html', context)
     
