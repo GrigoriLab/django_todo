82570cdc5adf9e17f00f29843344469d86a79944
Grigori Kartashyan <grigori.kartashyan@gmail.com>
2017-08-31 15:23:55 +0400

    Add html files

--- todos/urls.py
+++ todos/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
         url(r'^$', views.index, name='index'),
+        url(r'^details/(?P<id>\w{0,50})/$', views.details, name='details'),
+        url(r'^add/(?P<id>\w{0,50})/$', views.add, name='add'),
         ]
