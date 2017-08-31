1ca57163efb7e096f7d134ff23446bfd8d266b9c
Grigori Kartashyan <grigori.kartashyan@gmail.com>
2017-08-31 15:37:22 +0400

    Add todo adding functionality

--- todos/urls.py
+++ todos/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
         url(r'^$', views.index, name='index'),
         url(r'^details/(?P<id>\w{0,50})/$', views.details, name='details'),
-        url(r'^add/(?P<id>\w{0,50})/$', views.add, name='add'),
+        url(r'^add/', views.add, name='add'),
         ]
