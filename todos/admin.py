5906eda6ad8e849dc56df8ae98261fb1f2a08500
Grigori Kartashyan <grigori.kartashyan@gmail.com>
2017-09-06 11:27:38 +0400

    Add extra fields and search field

--- todos/admin.py
+++ todos/admin.py
@@ -4,7 +4,9 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import Todo, TinyTrackerReport
 
-admin.site.register(Todo)
-admin.site.register(TinyTrackerReport)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title','text','created_at')
+    search_fields = ['title']
 
-# Register your models here.
+admin.site.register(Todo,TodoAdmin)
+admin.site.register(TinyTrackerReport)
